fc59565537d9f4fe082286081ab0aa0aa7ef25de
manojp1988 <manojp1988@gmail.com>
2017-01-12 19:35:15 -0500

    spring ws boot working sample

---
+++ webservices/springWs/src/main/java/com/example/WebServiceConfig.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
+
+/**
+ * 
+ * @author manojperiathambi
+ *
+ */
+//This annotation helps to find the class annotated with @EndPoint and deliver the request to that class.
+@EnableWs
+@Configuration
+public class WebServiceConfig extends WsConfigurerAdapter{
+	
+	@Bean
+    public ServletRegistrationBean messageDispatcherServlet(
+            ApplicationContext applicationContext) {
+
+        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+        servlet.setApplicationContext(applicationContext);
+
+        return new ServletRegistrationBean(servlet,
+                "/springws/ws/*");
+    }
+
+	//the value of name attribute is used to create uri for the wsdl.
+    @Bean(name = "helloworld")
+    public Wsdl11Definition defaultWsdl11Definition() {
+        SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+        wsdl11Definition.setWsdl(
+                new ClassPathResource("/wsdl/helloworld.wsdl"));
+
+        return wsdl11Definition;
+    }
+
+}
