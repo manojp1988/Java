2147dccdb9fdc468ed4ce7030de0b04c8c580564
manojp1988 <manojp1988@gmail.com>
2017-01-14 08:59:58 -0500

    secured soap services with password authentication

--- webservices/springWs/src/main/java/com/example/WebServiceConfig.java
+++ webservices/springWs/src/main/java/com/example/WebServiceConfig.java
@@ -1,5 +1,8 @@
 package com.example;
 
+import java.util.List;
+import java.util.Properties;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.EndpointInterceptor;
+import org.springframework.ws.soap.security.wss4j.Wss4jSecurityInterceptor;
+import org.springframework.ws.soap.security.wss4j.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
@@ -16,30 +22,53 @@ import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
  * @author manojperiathambi
  *
  */
-//This annotation helps to find the class annotated with @EndPoint and deliver the request to that class.
+// This annotation helps to find the class annotated with @EndPoint and deliver
+// the request to that class.
 @EnableWs
 @Configuration
-public class WebServiceConfig extends WsConfigurerAdapter{
+public class WebServiceConfig extends WsConfigurerAdapter {
+
 	
 	@Bean
-    public ServletRegistrationBean messageDispatcherServlet(
-            ApplicationContext applicationContext) {
+	public SimplePasswordValidationCallbackHandler securityCallbackHandler(){
+		SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+        Properties users = new Properties();
+        users.setProperty("admin", "secret");
+        callbackHandler.setUsers(users);
+        return callbackHandler;
+	}
 
-        MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-        servlet.setApplicationContext(applicationContext);
+	
+	@Bean
+	public Wss4jSecurityInterceptor wss4jSecurityInterceptor(){
+		Wss4jSecurityInterceptor securityInterceptor = new Wss4jSecurityInterceptor();
+        securityInterceptor.setValidationActions("Timestamp UsernameToken");
+        securityInterceptor.setValidationCallbackHandler(securityCallbackHandler());
+        return securityInterceptor;
+	}
+	
+	
+	@Override
+	public void addInterceptors(List<EndpointInterceptor> interceptors) {
+		interceptors.add(wss4jSecurityInterceptor());
+	}
+	
+	@Bean
+	public ServletRegistrationBean messageDispatcherServlet(ApplicationContext applicationContext) {
 
-        return new ServletRegistrationBean(servlet,
-                "/springws/ws/*");
-    }
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
 
-	//the value of name attribute is used to create uri for the wsdl.
-    @Bean(name = "helloworld")
-    public Wsdl11Definition defaultWsdl11Definition() {
-        SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
-        wsdl11Definition.setWsdl(
-                new ClassPathResource("/wsdl/helloworld.wsdl"));
+		return new ServletRegistrationBean(servlet, "/springws/ws/*");
+	}
 
-        return wsdl11Definition;
-    }
+	// the value of name attribute is used to create uri for the wsdl.
+	@Bean(name = "helloworld")
+	public Wsdl11Definition defaultWsdl11Definition() {
+		SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+		wsdl11Definition.setWsdl(new ClassPathResource("/wsdl/helloworld.wsdl"));
+
+		return wsdl11Definition;
+	}
 
 }
