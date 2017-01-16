c1bac093a7e42b19886d5bd50b46b36910cf4710
manojp1988 <manojp1988@gmail.com>
2017-01-16 11:22:19 -0500

    Spring rest without boot

---
+++ webservices/rest/spring4-restful-example-master/src/main/java/net/viralpatel/spring/config/AppInitializer.java
@@ -0,0 +1,26 @@
+package net.viralpatel.spring.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/*
+ * This is the replacement for web.xml
+ * 
+ */
+public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class[] getRootConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected Class[] getServletConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
