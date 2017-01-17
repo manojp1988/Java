4b8d688b6114c392e624cbbbbc91998e9199efd8
manojp1988 <manojp1988@gmail.com>
2017-01-17 17:43:45 -0500

    Comments for security config

--- webservices/rest/spring4-restful-example-master/src/main/java/net/viralpatel/spring/config/SecurityWebApplicationInitializer.java
+++ webservices/rest/spring4-restful-example-master/src/main/java/net/viralpatel/spring/config/SecurityWebApplicationInitializer.java
@@ -2,6 +2,10 @@ package net.viralpatel.spring.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
+/*
+ * 
+ * This registers springSecurityFilterChain in the app.
+ */
 public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer{
 
 }
