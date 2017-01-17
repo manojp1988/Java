2965f1b201174e2e4fed2d4ec97d86a0fed07ab6
manojp1988 <manojp1988@gmail.com>
2017-01-17 17:41:27 -0500

    spring security basic authentication

---
+++ webservices/rest/spring4-restful-example-master/src/main/java/net/viralpatel/spring/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package net.viralpatel.spring.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private static String REALM="MY_TEST_REALM";
+    
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("password").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("tom").password("password").roles("USER");
+    }
+     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+  
+      http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers("/customers/**").hasRole("ADMIN")
+        .and().httpBasic().realmName(REALM).authenticationEntryPoint(getBasicAuthEntryPoint())
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
+    }
+     
+    @Bean
+    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
+        return new CustomBasicAuthenticationEntryPoint();
+    }
+
+}
